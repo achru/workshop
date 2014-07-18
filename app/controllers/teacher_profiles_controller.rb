3cdb947923e13748fcd6ac1a40ca7694c93d107a
Prelang Builder <builder@prelang.com>
2014-07-18 08:16:03 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/teacher_profiles_controller.rb
+++ app/controllers/teacher_profiles_controller.rb
@@ -1,4 +1,8 @@
 class TeacherProfilesController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_teacher_profile, only: [:show, :edit, :update, :destroy]
 
   # GET /teacher_profiles
